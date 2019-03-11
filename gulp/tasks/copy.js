e809fa1e05b40e890b546b6937aa154553f4a265
PixelsCommander <dradin@evolutiongaming.com>
2019-03-11 22:15:13 +0100

    Added deploy.js

---
+++ gulp/tasks/copy.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp');
+var config = require('../config.js');
+
+gulp.task('copy:fonts', function () {
+    return gulp
+        .src(config.src.fonts + '/*.{ttf,eot,woff,woff2}')
+        .pipe(gulp.dest(config.dest.fonts));
+});
+
+gulp.task('copy:data', function () {
+    return gulp
+        .src(config.src.data + '/**/*.*')
+        .pipe(gulp.dest(config.dest.data));
+});
+
+gulp.task('copy:lib', function () {
+    return gulp
+        .src(config.src.lib + '/**/*.*')
+        .pipe(gulp.dest(config.dest.lib));
+});
+
+gulp.task('copy:rootfiles', function () {
+    return gulp
+        .src(config.src.root + '/*.*')
+        .pipe(gulp.dest(config.dest.root));
+});
+
+gulp.task('copy:img', function () {
+    return gulp
+        .src([
+            config.src.img + '/**/*.{jpg,png,jpeg,svg,gif,ico}',
+            '!' + config.src.img + '/svgo/**/*.*'
+        ])
+        .pipe(gulp.dest(config.dest.img));
+});
+gulp.task('copy:video', function () {
+    return gulp
+        .src([
+            config.src.video + '/*.{mp4,webm,mov}'
+        ])
+        .pipe(gulp.dest(config.dest.video));
+});
+
+gulp.task('copy', [
+    'copy:img',
+    'copy:video',
+    'copy:rootfiles',
+    // 'copy:lib',
+    // 'copy:data',
+    'copy:fonts'
+]);
+gulp.task('copy:watch', function () {
+    gulp.watch(config.src.img + '/*', ['copy']);
+});
