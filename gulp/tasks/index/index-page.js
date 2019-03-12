b1f29ef4f3afc08b32ed52dc04ff13856b0f656d
Robert Haritonov <r@rhr.me>
2019-03-12 18:19:52 +0000

    update page structure and fix deps

--- gulp/tasks/index/index-page.js
+++ gulp/tasks/index/index-page.js
@@ -7,7 +7,7 @@ gulp.task('list-pages', function() {
 	delete require.cache[require.resolve('../../../' + config.src.pagelist)]
     var pages = require('../../../' + config.src.pagelist);
     return gulp
-        .src(__dirname + '/index.html')
+        .src(__dirname + '/pages.html')
         .pipe(consolidate('lodash', {
             pages: pages
         }))
