42b658027df99658ae35618d0d4c2514797261bd
PixelsCommander <denis.radin@gmail.com>
2019-09-14 15:32:50 +0200

    Deploy for 2019

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -4,7 +4,7 @@ var minimist = require('minimist');
 var gutil = require('gulp-util');
 var args = minimist(process.argv.slice(2));
 
-gulp.task('deploy', function() {
+gulp.task('deploy', function () {
   var remotePath = '/';
   var conn = ftp.create({
     host: 'gold.elastictech.org',
@@ -26,10 +26,10 @@ gulp.task('deploy', function() {
     .pipe(conn.dest(`${remotePath}/sponsors`));
 
   // uncomment to deploy last year versions
-  // gulp.src([
-  //   './2018/**/*.*'
-  // ])
-  //   .pipe(conn.dest(`${remotePath}/2018`));
+  gulp.src([
+    './2019/**/*.*'
+  ])
+    .pipe(conn.dest(`${remotePath}/2019`));
   //
   // gulp.src([
   //   './2017/**/*.*'
