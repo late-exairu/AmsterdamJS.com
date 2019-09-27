05de22127b3e84f30f3d26273db992c00ee41997
Robert Haritonov <r@rhr.me>
2019-09-27 11:51:49 +0100

    remove 2019 upload

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -26,10 +26,10 @@ gulp.task('deploy', function () {
     .pipe(conn.dest(`${remotePath}/sponsors`));
 
   // uncomment to deploy last year versions
-  gulp.src([
-    './2019/**/*.*'
-  ])
-    .pipe(conn.dest(`${remotePath}/2019`));
+  // gulp.src([
+  //   './2019/**/*.*'
+  // ])
+  //   .pipe(conn.dest(`${remotePath}/2019`));
   //
   // gulp.src([
   //   './2017/**/*.*'
