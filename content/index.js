2999e974d86d74f255f79a4cd35342d02ee63c2c
Oleg Proskurin <regx@usul.su>
2019-11-19 22:32:08 +0300

    Add FAQ

--- content/index.js
+++ content/index.js
@@ -9,6 +9,7 @@ const sponsorContent = require('./fetch-sponsors');
 const talksContent = require('./fetch-talks');
 const workshopContent = require('./fetch-workshops');
 const mcContent = require('./fetch-mc');
+const faqContent = require('./fetch-faq');
 
 const createClient = ({ endpoint, token }) => {
   return new GraphQLClient(endpoint, {
@@ -30,6 +31,7 @@ const getContent = async () => {
     talksContent,
     workshopContent,
     mcContent,
+    faqContent,
   ].map(async content => {
     try {
       return await content.fetchData(client, { conferenceTitle, eventYear });
